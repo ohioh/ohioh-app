0679336adb2d124d05c304f541703b15c59c13d3
Alam Al Saud <alamalsaud5@gmail.com>
2020-11-29 23:35:05 +0600

    new login js

--- js/login.js
+++ js/login.js
@@ -1,27 +1,39 @@
-     function submitInfo(){
-    
-    let form = document.forms["myForm"];
-    let fd = new FormData(form);
-    let data = {};
-    for (let [key, prop] of fd) {
-      data[key] = prop;
-    }
-    VALUE = JSON.stringify(data, null, 2);
-    console.log(VALUE);
+function setCookie(cvalue) {
+  var d = new Date();
+  d.setTime(d.getTime() + 1000 * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = "email" + "=" + cvalue + ";" + expires + ";path=/";
+}
+var url =
+  "http://195.201.35.222:3000/login?email=Mbadhon@gmail.com&password=badhon";
+function submitInfo() {
+  let form = document.forms["myForm"];
+  let fd = new FormData(form);
+  let data = {};
+  for (let [key, prop] of fd) {
+    data[key] = prop;
+  }
+  VALUE = JSON.stringify(data, null, 2);
+  console.log(VALUE);
 
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    fetch('http//195.201.35.222:3000/login', {
-        method: 'POST',
-        headers: myHeaders,
-        mode: 'cors',
-        cache: 'default',
-        body: VALUE
+  fetch("https://app.ohioh.de:8443/login", {
+    method: "POST",
+    headers: myHeaders,
+    mode: "cors",
+    cache: "default",
+    body: VALUE,
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+      setCookie(fd.get("email"));
+      window.location.href =
+        "https://serene-panini-dac406.netlify.app/index.html";
     })
-    .then(data => data.json())
-    .then(data =>  { console.log(data) }) 
     .catch((err) => {
-        console.error(err);
-    })
+      console.error(err);
+    });
 }
